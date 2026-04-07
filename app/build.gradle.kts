ea31d9675fd58d209e78a995743e63ba44ab4fa0
jt <jt@jt.com>
2026-04-07 11:31:18 -0400

    fix keyboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vocabularyquest.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 11
+        versionName = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
